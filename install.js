3434f7164381a0cc102e854f46413a943b0cdde9
Vale <changweitu@gmail.com>
2015-10-19 11:35:27 +0800

    modify install.js

--- install.js
+++ install.js
@@ -4,14 +4,12 @@ var dir = process.argv[2];
 var exec = require('child_process').exec;
 fs.readdir(dir,function(error,files){
     for (var i=0; i< files.length; i++) {
-        if (files[i] != "README.md") {
-            var path = dir + files[i];
-            exec("xcodesnippet install " + path, function(err,stdout,stderror){
-                if(err) throw err;
-                console.log(stdout);
-            });
-        }
-
+        if (files[i] == "README.md" || files[i] == "LICENSE") continue;
+        var path = dir + "/" + files[i];
+        exec("xcodesnippet install " + path, function(err,stdout,stderror){
+            if(err) throw err;
+            console.log(stdout);
+        });
     }
 
 });
